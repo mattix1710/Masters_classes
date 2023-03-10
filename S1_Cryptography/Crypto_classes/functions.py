7f45f6b85baefcdfd812752d5a72c4d514d2ba89
Mateusz Szuda <64504618+mattix1710@users.noreply.github.com>
2023-03-11 00:24:52 +0100

    Added prime numbers finder fun

--- S1_Cryptography/Crypto_classes/functions.py
+++ S1_Cryptography/Crypto_classes/functions.py
@@ -18,5 +18,22 @@ def gcd_euclidean(num1: int, num2: int):
     
     # the remainder of divising both numbers is calculated and passed through recurrency
     return gcd_euclidean(num1%num2, num2)
+
+# function for finding all prime numbers from given range based on Erathosthenes' sieve
+def prime_finder(maxVal: int):
+    numbers = []
+    for enum in range(2, maxVal):
+        numbers.append(enum)
     
-print(gcd_euclidean(0, 46))
+    for num, it in zip(numbers, range(0, len(numbers))):
+        if num == 0:
+            continue
+        for higherVals, it2 in zip(numbers[it+1:], range(it+1, len(numbers))):
+            if higherVals % num == 0:
+                numbers[it2] = 0
+    
+    return list(filter((0).__ne__, numbers))
+    
+# print(gcd_euclidean(0, 46))
+
+print(prime_finder(200))
